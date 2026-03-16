4ec83d424ae550687cb14aab763614bfb39da8fb
wtomoki1102-crypto <wtomoki1102@gmail.com>
2026-03-16 22:11:48 +0900

    割り算

---
+++ ITP1/ITP1_4_A.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+  
+  int a,b;
+  scanf("%d%d",&a,&b);
+
+  int d=a/b;
+  int r=a%b;
+  double f= (double)a/b;
+
+  printf("%d %d %.5f\n",d,r,f);
+
+  return 0; 
+  
+}
