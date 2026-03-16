bfec079da64cf0576b18f0c60015a574605c52f8
wtomoki1102-crypto <wtomoki1102@gmail.com>
2026-03-16 17:13:58 +0900

    長方形の面積と周の長さ

---
+++ ITP1/ITP1_1_C.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+  int a,b;
+  scanf("%d%d",&a,&b);
+
+  int mennseki,syuu;
+
+  mennseki=a*b;
+  syuu=2*a+2*b;
+
+  printf("%d %d\n",mennseki,syuu);
+
+  return 0;
+     
+}
